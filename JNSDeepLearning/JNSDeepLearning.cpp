799aa4c7d222fbe7d009ca1ecef31f674096d468
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-03-11 00:11:17 +0900

    Initial commit

---
+++ JNSDeepLearning/JNSDeepLearning.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <cuda_runtime.h>
+
+__global__ void printHelloCUDA()
+{
+    printf("Hello CUDA!\n");
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    printHelloCUDA << <1, 1 >> > ();
+}
