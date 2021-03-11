7aebe63975fd885d66f2ca41b18684fce4c457f5
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-03-11 12:59:09 +0900

    주석 추가

--- JNSDeepLearning/JNSDeepLearning.cpp
+++ JNSDeepLearning/JNSDeepLearning.cpp
@@ -9,5 +9,5 @@ __global__ void printHelloCUDA()
 int main()
 {
     std::cout << "Hello World!\n";
-    printHelloCUDA << <1, 1 >> > ();
+    printHelloCUDA << <1, 1 >> > (); //1개 블럭을 생성하고 블럭마다 1개의 쓰레드를 생성한다.
 }
