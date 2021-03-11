8857fa6758a5ae00b6b9d66a75cb71ab08139db1
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-03-11 16:13:14 +0900

    코드 분석, 주석 추가

--- JNSDeepLearning/JNSDeepLearning.cpp
+++ JNSDeepLearning/JNSDeepLearning.cpp
@@ -19,7 +19,7 @@ int main()
     std::cout << "Hello World!\n";
     printHelloCUDA << <1, 1 >> > (); //<<<그리드 당 블록 수, 블록당 스레드 수>>>
     
-    float *A, *B, *C;
+    float *A, *B, *C; //디바이스 메모리 - GPU 쪽 메모리 
     int N = 50;
     cudaMalloc((void**)&A, N*N*sizeof(float));
     cudaMalloc((void**)&B, N*N*sizeof(float));
@@ -33,7 +33,7 @@ int main()
     cudaMemcpy(B, b, N*N*sizeof(*B), cudaMemcpyHostToDevice);
     
     dim3 ThreadsPBlock(N, N);
-    MatrixAdd<<<1, ThreadPBlock>>>(A, B, C);
+    MatrixAdd<<<1, ThreadPBlock>>>(A, B, C); //블록도 dim3로 정의할 수 있음, 블록을 묶은 것을 '그리드'라고 부름.
     
     cudaMemcpy(c, C, N*N*sizeof(*C), cudaMemcpyHostToDevice);
     
