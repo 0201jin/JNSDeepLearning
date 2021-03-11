faa2399408e85792ca43fb08b5fbb7fab322d0b9
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-03-11 15:56:11 +0900

    CUDA를 이용한 행렬합 연산 코드

--- JNSDeepLearning/JNSDeepLearning.cpp
+++ JNSDeepLearning/JNSDeepLearning.cpp
@@ -6,8 +6,38 @@ __global__ void printHelloCUDA()
     printf("Hello CUDA!\n");
 }
 
+__global__ void MatrixAdd(float A[N][N], float B[N][N], float C[N][N])
+{
+    int i = threadIdx.x;
+    int j = threadIdx.y;
+    
+    C[i][j] = A[i][j] + B[i][j];
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
     printHelloCUDA << <1, 1 >> > (); //1개 블럭을 생성하고 블럭마다 1개의 쓰레드를 생성한다.
+    
+    float *A, *B, *C;
+    int N = 50;
+    cudaMalloc((void**)&A, N*N*sizeof(float));
+    cudaMalloc((void**)&B, N*N*sizeof(float));
+    cudaMalloc((void**)&C, N*N*sizeof(float));
+    
+    float *a = malloc(N*N*sizeof(float));
+    float *b = malloc(N*N*sizeof(float));
+    float *c = malloc(N*N*sizeof(float));
+    
+    cudaMemcpy(A, a, N*N*sizeof(*A), cudaMemcpyHostToDevice);
+    cudaMemcpy(B, b, N*N*sizeof(*B), cudaMemcpyHostToDevice);
+    
+    dim3 ThreadsPBlock(N, N);
+    MatrixAdd<<<1, ThreadPBlock>>>(A, B, C);
+    
+    cudaMemcpy(c, C, N*N*sizeof(*C), cudaMemcpyHostToDevice);
+    
+    cudaFree(A);
+    cudaFree(B);
+    cudaFree(C);
 }
