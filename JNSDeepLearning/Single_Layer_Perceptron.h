52c9af5a69e5164e461ff2e5eae142c3a9f82fc9
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-03-12 16:54:13 +0900

    헤더파일 및 함수 추가

--- JNSDeepLearning/Single_Layer_Perceptron.h
+++ JNSDeepLearning/Single_Layer_Perceptron.h
@@ -1,13 +1,18 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <map>
+#include <random>
 
 using namespace std;
 
 class Neuron
 {
 public:
-  Neuron(size_t input_size);
+  Neuron(size_t _input_size);
+  double Compute(const std::vector<double>& _x) const;
+  void Train(double _a, const vector<pair<vector<double>, double>>& _train_data);
   
 private:
   void Reset();
