364da750c749c7d79ed88fddcbea8375e4bf064f
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-03-12 11:24:20 +0900

    ReLU 추가

--- JNSDeepLearning/Perceptron.h
+++ JNSDeepLearning/Perceptron.h
@@ -2,12 +2,13 @@
 #include <iostream>
 
 /*
-* �ܼ� �ۼ�Ʈ��
+* ´Ü¼ø ÆÛ¼ÁÆ®·Ð
 */
 namespace Perceptron
 {
 	float Dot(float* _v1, float* _v2, int _len);
 	float Step(float _v);
+	float ReLU(float _v);
 	float Forward(float *_x, float *_w, int _len);
 	float Train(float *_w, float *_x, float _t, float _e, int _len);
 };
