ca5943b73a8291e854f70dd894f1f952fdaa0b8b
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-03-12 17:11:07 +0900

    함수 작업

--- JNSDeepLearning/Single_Layer_Perceptron.cpp
+++ JNSDeepLearning/Single_Layer_Perceptron.cpp
@@ -1,2 +1,55 @@
 include "Single_Layer_Perceptron.h"
   
+Neuron::Neuron(size_t _input_size)
+{
+  m_vWeight.resize(_input_size);
+  Reset();
+}
+
+void Neuron::Reset()
+{
+  m_dBias = -1;
+ 
+  mt19937 random((random_device()()));
+  uniform_real_distribution<double> dist(-1, 1);
+ 
+  for (size_t i = 0; i < m_vWeight.size(); ++i)
+  {
+    m_vWeight[i] = dist(random);
+  }
+}
+
+double Neuron::Calculate(const vector<double>& _x) const
+{
+  if (x.size() != m_vWeight.size())
+    cout << "x.size() != Weight.size()" << endl;
+ 
+  double wx = 0.0;
+  for (size_t i = 0; i < m_vWeight.size(); ++i)
+  {
+      wx += m_vWeight[i] * x[i];
+  }
+ 
+  return ReLU(wx + m_dBias);
+}
+
+void Neuron::Train(double _a, const vector<pair<vector<double>, double>>& _train_data)
+{
+  size_t input_size = _train_data[0].first.size();
+  
+  if (input_size != m_vWeight.size())
+      cout << "input_size != Weights_.size()" << endl;
+ 
+  for (size_t i = 0; i < _train_data.size(); ++i)
+  {
+      double o = Calculate(_train_data[i].first);
+      double t = _train_data[i].second;
+ 
+      for (size_t j = 0; j < input_size; ++j)
+      {
+          m_vWeight[j] += a * (t - o) * _train_data[i].first[j];
+      }
+    
+      m_dBias += a * (t - o);
+  }
+}
