abafde054dc6ebc7cb81cf225ed36949e432790c
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-03-12 17:02:00 +0900

    활성화 함수 추가

--- JNSDeepLearning/Single_Layer_Perceptron.h
+++ JNSDeepLearning/Single_Layer_Perceptron.h
@@ -7,6 +7,21 @@
 
 using namespace std;
 
+double Sigmoid(double _x)
+{
+  return 1 / (1 + exp(-_x));
+}
+
+double Step(double _x)
+{
+  return _x > 0 ? 1 : 0;
+}
+
+double ReLU(double _x)
+{
+  return _x > 0 ? x : 0;
+}
+
 class Neuron
 {
 public:
