8fd128e9db7a74df406dbb8f5270d0d07b6c9fcf
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-03-12 13:05:01 +0900

    Sigmoid 추가

--- JNSDeepLearning/Perceptron.h
+++ JNSDeepLearning/Perceptron.h
@@ -1,14 +1,12 @@
 #pragma once
 #include <iostream>
 
-/*
-* ´Ü¼ø ÆÛ¼ÁÆ®·Ð
-*/
 namespace Perceptron
 {
 	float Dot(float* _v1, float* _v2, int _len);
 	float Step(float _v);
 	float ReLU(float _v);
+	float Sigmoid(float _v);
 	float Forward(float *_x, float *_w, int _len);
 	float Train(float *_w, float *_x, float _t, float _e, int _len);
 };
