26c5d98750162aa44e9f191bdc94ac519c7ba0f7
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-05-22 16:11:36 +0900

    테스트 전 커밋

--- JNSDeepLearning/LSTM/LSTM_Layer/LSTM_Layer.cpp
+++ JNSDeepLearning/LSTM/LSTM_Layer/LSTM_Layer.cpp
@@ -2,16 +2,8 @@
 
 LSTM_Layer::LSTM_Layer()
 {
-	random_device rd;
-	mt19937 random(rd());
-	uniform_real_distribution<double> dist(-1, 1);
-
-	for (int i = 0; i < 4; ++i)
-	{
-		m_dXWeight[i] = dist(random);
-		m_dHWeight[i] = dist(random);
-		m_dBias[i] = -1;
-	}
+	m_dXWeight.Init(); 
+	m_dHWeight.Init();
 
 	ClearLayer();
 }
@@ -20,12 +12,62 @@ void LSTM_Layer::ClearLayer()
 {
 	Mem_Gate.clear();
 	Mem_CH.clear();
+	m_vY.clear();
 
-	Mem_Gate.push_back(vector<double>({ 0, 0, 0, 0 }));
+	Mem_Gate.push_back(Gate());
 	Mem_CH.push_back(pair<double, double>(0, 0));
+}
+
+vector<double> LSTM_Layer::Calculate_M2M(vector<double> _InputData)
+{
+	//wh * h + wx * x + b
+
+	for (int i = 0; i < _InputData.size(); ++i)
+	{
+		Gate gate;
+
+		gate.f = Sigmoid(m_dHWeight.f * Mem_CH[i].second + m_dXWeight.f * _InputData[i] + m_dBias.f);
+		gate.i = Sigmoid(m_dHWeight.i * Mem_CH[i].second + m_dXWeight.i * _InputData[i] + m_dBias.i);
+		gate.o = Sigmoid(m_dHWeight.o * Mem_CH[i].second + m_dXWeight.o * _InputData[i] + m_dBias.o);
+		gate.c = Tanh(m_dHWeight.c * Mem_CH[i].second + m_dXWeight.c * _InputData[i] + m_dBias.c);
+
+		double C = Mem_CH[i].first * gate.f + (gate.c * gate.i);
+		double H = gate.o * Tanh(C);
+
+		Mem_Gate.push_back(gate);
+		Mem_CH.push_back(pair<double, double>(C, H));
+
+		m_vY.push_back(H);
+	}
+
+	return m_vY;
+}
+
+void LSTM_Layer::Train_M2M(vector<double> _InputData, vector<double> _TrainData)
+{
+	vector<double> Y = Calculate_M2M(_InputData);
+
+	for (int i = 0; i < _InputData.size(); ++i)
+	{
+		double dy = 2 * (Y[i] - _TrainData[i]);
+		double dh = dy;
+	}
 }
 
 
 LSTM_Network::LSTM_Network()
 {
+}
+
+vector<double> LSTM_Network::Calculate_M2M(vector<double> _InputData)
+{
+	return m_Layer.Calculate_M2M(_InputData);
+}
+
+void LSTM_Network::Train_M2M(vector<vector<double>> _InputData, vector<vector<double>> _TrainData)
+{
+	for (int i = 0; i < _InputData.size(); ++i)
+	{
+		m_Layer.Train_M2M(_InputData[i], _TrainData[i]);
+	}
 }
--- JNSDeepLearning/LSTM/LSTM_Layer/LSTM_Layer.h
+++ JNSDeepLearning/LSTM/LSTM_Layer/LSTM_Layer.h
@@ -11,6 +11,26 @@
 
 using namespace Activation_Function;
 
+struct Gate
+{
+	double f = -1;
+	double i = -1;
+	double c = -1;
+	double o = -1;
+
+	void Init()
+	{
+		random_device rd;
+		mt19937 random(rd());
+		uniform_real_distribution<double> dist(-1, 1);
+
+		f = dist(random);
+		i = dist(random);
+		c = dist(random);
+		o = dist(random);
+	}
+};
+
 class LSTM_Layer
 {
 public:
@@ -18,24 +38,26 @@ public:
 
 	void ClearLayer();
 
+	vector<double> Calculate_M2M(vector<double> _InputData);
+	void Train_M2M(vector<double> _InputData, vector<double> _TrainData);
+
 private:
-	double m_dXWeight[4] = { 0, 0, 0, 0 };
-	double m_dHWeight[4] = { 0, 0, 0, 0 };
-	double m_dBias[4] = {-1, -1, -1, -1};
-
-	double m_VWeight = 1;
-	double m_VBias = 0;
-
-	double c, h;
+	Gate m_dXWeight;
+	Gate m_dHWeight;
+	Gate m_dBias;
 
 	vector<pair<double, double>> Mem_CH;
-	vector<vector<double>> Mem_Gate;
+	vector<Gate> Mem_Gate;
+	vector<double> m_vY;
 };
 
 class LSTM_Network
 {
 public:
 	LSTM_Network();
+
+	vector<double> Calculate_M2M(vector<double> _InputData);
+	void Train_M2M(vector<vector<double>> _InputData, vector<vector<double>> _TrainData);
 
 private:
 	LSTM_Layer m_Layer;
