4b6e20e749ed870718ff124a35364ff52280b1c8
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-05-26 11:04:29 +0900

    DeepLSTM 기초 작업

--- JNSDeepLearning/LSTM/LSTM_Layer/DeepLSTM/DeepLSTM.h
+++ JNSDeepLearning/LSTM/LSTM_Layer/DeepLSTM/DeepLSTM.h
@@ -3,5 +3,10 @@
 
 class DeepLSTM
 {
-
+public:
+  double Calculate_M2O(vector<double> _InputData);
+  void Train_M2O(vector<double> _InputData, double _TrainData);
+  
+private:
+  vector<LSTM_Neuron> m_vNeuron;
 };
