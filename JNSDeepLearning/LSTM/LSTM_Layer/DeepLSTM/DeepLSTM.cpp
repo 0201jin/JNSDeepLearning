935cdab63d49b2f26c5a56611cf38a3a0fb1f6f1
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-05-26 14:08:10 +0900

    Update DeepLSTM.cpp

--- JNSDeepLearning/LSTM/LSTM_Layer/DeepLSTM/DeepLSTM.cpp
+++ JNSDeepLearning/LSTM/LSTM_Layer/DeepLSTM/DeepLSTM.cpp
@@ -1,2 +1,23 @@
 #include "DeepLSTM.h"
 
+DeepLSTM::DeepLSTM(int _NeuronSize)
+{
+  for(int i = 0; i < _NeuronSize; ++i)
+  {
+    m_vNeuron.push_back(LSTM_Neuron);
+  }
+}
+
+double DeepLSTM::Calculate_M2O(vector<double> _InputData)
+{
+  vector<double> Cal = _InputData;
+  
+  for(vector<LSTM_Neuron>::iterator iter = m_vNeuron.begin(); iter != m_vNeuron.end() - 1; ++iter)
+  {
+    
+  }
+}
+
+void DeepLSTM::Train_M2O(vector<double> _InputData, double _TrainData)
+{
+}
