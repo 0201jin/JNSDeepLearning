ad7785d1b9a949695cdd282d476057fd96d3b7bd
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-07-06 09:49:22 +0900

    Update DeepLSTM.cpp

--- JNSDeepLearning/LSTM/LSTM_Layer/DeepLSTM/DeepLSTM.cpp
+++ JNSDeepLearning/LSTM/LSTM_Layer/DeepLSTM/DeepLSTM.cpp
@@ -22,15 +22,18 @@ double DeepLSTM::Calculate_M2O(vector<double> _InputData)
 
 void DeepLSTM::Train_M2O(vector<vector<double>> _InputData, vector<double> _TrainData)
 {
+	static m = 0;
+	static v = 0;
+	
 	for (int i = 0; i < _InputData.size(); ++i)
 	{
 		Calculate_M2O(_InputData[i]);
 
-		queue<double> TrainData = m_vNeuron[m_vNeuron.size() - 1].Train_Y(m_vNeuron[m_vNeuron.size() - 1].GetLastInput(), _TrainData[i], 0.00025);
+		queue<double> TrainData = m_vNeuron[m_vNeuron.size() - 1].Train_Y(m_vNeuron[m_vNeuron.size() - 1].GetLastInput(), _TrainData[i], 0.00025, &m, &v);
 
 		for (int j = m_vNeuron.size() - 2; j >= 0; --j)
 		{
-			TrainData = m_vNeuron[j].Train_H(m_vNeuron[j].GetLastInput(), TrainData, 0.00025);
+			TrainData = m_vNeuron[j].Train_H(m_vNeuron[j].GetLastInput(), TrainData, 0.00025, &m, &v);
 		}
 	}
 }
