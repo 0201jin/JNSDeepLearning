70177738348a9158e55a041decfcf1fa044835f7
JiNyeok_S <53131813+0201jin@users.noreply.github.com>
2021-07-12 15:32:30 +0900

    M2M 연산 함수 수정

--- JNSDeepLearning/LSTM/LSTM_Layer/LSTM_Layer.cpp
+++ JNSDeepLearning/LSTM/LSTM_Layer/LSTM_Layer.cpp
@@ -83,76 +83,15 @@ void LSTM_Neuron::ClearLayer()
 
 vector<double> LSTM_Neuron::Calculate_M2M(vector<double> _InputData)
 {
-	ClearLayer();
+	vector<double> Y = Calculate_H(_InputData);
 
-	for (int i = 0; i < _InputData.size(); ++i)
-	{
-		Gate gate;
-
-		gate.f = Sigmoid(m_XWeight.f * _InputData[i] + m_HWeight.f * Mem_CH[i].H + m_HBias.f);
-		gate.i = Sigmoid(m_XWeight.i * _InputData[i] + m_HWeight.i * Mem_CH[i].H + m_HBias.i);
-		gate.c_ = Sigmoid(m_XWeight.c_ * _InputData[i] + m_HWeight.c_ * Mem_CH[i].H + m_HBias.c_);
-		gate.g = Tanh(m_XWeight.g * _InputData[i] + m_HWeight.g * Mem_CH[i].H + m_HBias.g);
-
-		CH ch;
-		ch.C = Mem_CH[i].C * gate.f + gate.i * gate.g;
-		ch.H = gate.c_ * Tanh(ch.C);
-
-		double Y = ch.H * m_YWeight + m_YBias;
-
-		Mem_CH.push_back(ch);
-		Mem_Gate.push_back(gate);
-		m_vY.push_back(Y);
-	}
-
-	return m_vY;
+	return Y[Y.size() - 1];
 }
 
 vector<double> LSTM_Neuron::Train_M2M(vector<double> _InputData, vector<double> _TrainData)
 {
-	vector<double> Y = Calculate_M2M(_InputData);
-	vector<double> dH;
-
-	CH prev_dCH;
-
-	for (int i = Y.size() - 1; i >= 0; --i)
-	{
-		double dy = 2 * (Y[i] - _TrainData[i]);
-		double dh = dy * m_YWeight + prev_dCH.H;
-		double dc = Tanh_Derivative(Mem_CH[i + 1].C) * dh * Mem_Gate[i].c_ + prev_dCH.C;
-
-		m_YWeight -= dy * Mem_CH[i + 1].H * LEARN_RATE;
-		m_YBias -= dy * LEARN_RATE;
-
-		Gate gate;
-
-		gate.f = Mem_CH[i].C * dc * Sigmoid_Derivative(Mem_Gate[i].f);
-		gate.i = Mem_Gate[i].g * dc * Sigmoid_Derivative(Mem_Gate[i].i);
-		gate.g = Mem_Gate[i].i * dc * Sigmoid_Derivative(Mem_Gate[i].g);
-		gate.c_ = Tanh(Mem_CH[i + 1].C) * dh * Tanh_Derivative(Mem_Gate[i].c_);
-
-		m_XWeight.f -= gate.f * _InputData[i] * LEARN_RATE;
-		m_XWeight.i -= gate.i * _InputData[i] * LEARN_RATE;
-		m_XWeight.g -= gate.g * _InputData[i] * LEARN_RATE;
-		m_XWeight.c_ -= gate.c_ * _InputData[i] * LEARN_RATE;
-
-		m_HWeight.f -= gate.f * Mem_CH[i].H * LEARN_RATE;
-		m_HWeight.i -= gate.i * Mem_CH[i].H * LEARN_RATE;
-		m_HWeight.g -= gate.g * Mem_CH[i].H * LEARN_RATE;
-		m_HWeight.c_ -= gate.c_ * Mem_CH[i].H * LEARN_RATE;
-
-		m_HBias.f -= gate.f * LEARN_RATE;
-		m_HBias.i -= gate.i * LEARN_RATE;
-		m_HBias.g -= gate.g * LEARN_RATE;
-		m_HBias.c_ -= gate.c_ * LEARN_RATE;
-
-		prev_dCH.C = Mem_Gate[i].f * dc;
-		prev_dCH.H = (gate.f + gate.i + gate.g + gate.c_) * (Mem_Gate[i].f + Mem_Gate[i].i + Mem_Gate[i].g + Mem_Gate[i].c_);
-
-		dH.push_back(prev_dCH.H);
-	}
-
-	return dH;
+	static double m, v = 0;
+	Train_H_Adam(_InputData, _TrainData, 0.00025, &m, &v);
 }
 
 double LSTM_Neuron::Calculate_M2O(vector<double> _InputData)
