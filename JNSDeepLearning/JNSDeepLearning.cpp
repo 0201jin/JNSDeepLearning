22d8dbc570b7dece8ac24ac700161a07ab01149d
0201jin <0201jin01@naver.com>
2021-11-27 19:44:47 +0900

    테스트

--- JNSDeepLearning/JNSDeepLearning.cpp
+++ JNSDeepLearning/JNSDeepLearning.cpp
@@ -104,7 +104,7 @@ void LSTM_M2O_Run()
 {
 	LSTM_Network<double> net;
 
-	for (int i = 0; i < 1000000; ++i)
+	for (int i = 0; i < 10000; ++i)
 	{
 		net.Train(
 			{ {0.1, 0.2, 0.3}, {0.4, 0.5, 0.6}, {0.7, 0.8},
@@ -139,8 +139,8 @@ int main()
 	auto start = chrono::system_clock::now();
 
 	//RNN_M2M_Run();
-	LSTM_M2O_Run();
-	//LSTM_M2M_Run();
+	//LSTM_M2O_Run();
+	LSTM_M2M_Run();
 
 	auto end = chrono::system_clock::now();
 
