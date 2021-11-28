b4391b2d548f0abd146d82ae7e8e777c4e28504f
0201jin <0201jin01@naver.com>
2021-11-28 20:55:37 +0900

    cal, train 간단 작업

--- JNSDeepLearning/LSTM/BiLSTM/BiLSTM.h
+++ JNSDeepLearning/LSTM/BiLSTM/BiLSTM.h
@@ -25,6 +25,17 @@ public:
 		//Attention Mechanism ÀÛ¾÷
 	}
 
+	void Train(const vector<T> _InputData, const vector<T> _Answer)
+	{
+		vector<T> Forward_Answer;
+		vector<T> Backward_Answer;
+
+		Forward_LSTM.Calculate(_InputData, Forward_Answer);
+		Backward_LSTM.Calculate(Vector_Reverse_Function(_InputData), Backward_Answer);
+
+		
+	}
+
 private:
 	LSTM_Neuron<T> Forward_LSTM;
 	LSTM_Neuron<T> Backward_LSTM;
