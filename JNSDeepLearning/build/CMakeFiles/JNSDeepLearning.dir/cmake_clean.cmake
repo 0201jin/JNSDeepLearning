d34afb33ddc9e0e0e933410ad7b646fa3fb1c1df
0201jin <0201jin01@naver.com>
2021-12-27 20:56:19 +0900

    CMake 작업

---
+++ JNSDeepLearning/build/CMakeFiles/JNSDeepLearning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JNSDeepLearning.dir/JNSDeepLearning.cpp.o"
+  "JNSDeepLearning"
+  "JNSDeepLearning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JNSDeepLearning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
